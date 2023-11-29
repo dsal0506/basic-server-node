f885c05e4d6a12131780501d3abe819cbfa84e6c
dsal0506 <dsalinas_5605@stu.southtexascollege.edu>
2023-11-29 13:17:46 -0600

    fixed

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -6,6 +6,10 @@ import departments from './departments'
 import employees from './employees'
 
 const router = Router()
+router.use(basicAuth({
+    users: {[process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD}
+}),
+)
 
 router.get('/', (req, res) => {
   res.send({ msg: 'Inside API Endpoints' })
